c146b5cce11b3b474184e1505ba5d8942a15c095
Rahul Kukadiya <rkukadiy@lakeheadu.ca>
2023-01-25 10:23:32 -0500

    Add argument query

--- src/main/scala/dao/AppDAO.scala
+++ src/main/scala/dao/AppDAO.scala
@@ -1,9 +1,19 @@
 package dao
 
-import schema.DBSchema
+import models.Patient
 import schema.DBSchema.patientList
 import slick.jdbc.H2Profile.api._
 
+import scala.concurrent.Future
+
 class AppDAO(db: Database) {
-  def patientLst = db.run(patientList.result)
+  def patientLst: Future[Seq[Patient]] = db.run(patientList.result)
+
+  def getPatient(id: Int): Future[Option[Patient]] = db.run(
+    patientList.filter(_.id === id).result.headOption
+  )
+
+  def getPatients(id: Seq[Int]): Future[Seq[Patient]] = db.run(
+    patientList.filter(_.id inSet id).result
+  )
 }
--- src/main/scala/schema/GraphQLSchema.scala
+++ src/main/scala/schema/GraphQLSchema.scala
@@ -14,13 +14,33 @@ object GraphQLSchema {
       Field("age", IntType, resolve = _.value.age)
     )
   )
+  private val ID_ARG_TYPE = Argument("id", IntType)
+  private val ID_LIST_ARG_TYPE = Argument("ids", ListInputType(IntType))
 
-  val QueryType = ObjectType(
+  private val queryType = ObjectType(
     "Query",
     fields[MyContext, Unit](
-      Field("patientList", ListType(patientType), resolve = c => c.ctx.dao.patientLst)
+      Field(
+        "patientList",
+        ListType(patientType),
+        resolve = c => c.ctx.dao.patientLst
+      ),
+
+      Field(
+        "patient",
+        OptionType(patientType),
+        arguments = ID_ARG_TYPE :: Nil,
+        resolve = config => config.ctx.dao.getPatient(config.arg(ID_ARG_TYPE))
+      ),
+
+      Field(
+        "patients",
+        OptionType(ListType(patientType)),
+        arguments = List(ID_LIST_ARG_TYPE),
+        resolve = config => config.ctx.dao.getPatients(config.arg(ID_LIST_ARG_TYPE))
+      )
     )
   )
 
-  val schemaDefinition: Schema[MyContext, Unit] = Schema(QueryType)
+  val schemaDefinition: Schema[MyContext, Unit] = Schema(queryType)
 }
